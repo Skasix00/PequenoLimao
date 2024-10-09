85c8a97b0b9ce148c8f834aa69fa7ecf9269d8af
Bruno Castelo <brunoalexcastelo@gmail.com>
2024-10-09 15:03:10 +0100

    navigation with arguments

--- ScaffoldEx/app/src/main/java/com/littlelemon/littlelemonmenu/MainActivity.kt
+++ ScaffoldEx/app/src/main/java/com/littlelemon/littlelemonmenu/MainActivity.kt
@@ -54,38 +54,84 @@ class MainActivity : ComponentActivity() {
 fun MenuContent(paddingValues: PaddingValues) {
     Surface(modifier = Modifier.padding(paddingValues)) {
         val menuPadding = 8.dp
-		Column {
-			Text(
-				"Appetizers",
-				modifier = Modifier
-					.weight(0.25f)
-					.background(Purple80)
-					.padding(menuPadding)
-					.fillMaxWidth()
-			)
-			Text(
-				"Salads",
-				modifier = Modifier
-					.weight(0.25f)
-					.padding(menuPadding)
-					.fillMaxWidth()
-			)
-			Text(
-				"Drinks",
-				modifier = Modifier
-					.weight(0.25f)
-					.background(Pink80)
-					.padding(menuPadding)
-					.fillMaxWidth()
-			)
-			Text(
-				"Mains",
-				modifier = Modifier
-					.weight(0.25f)
-					.background(PurpleGrey80)
-					.padding(menuPadding)
-					.fillMaxWidth()
-			)
-		}
+        val configuration = LocalConfiguration.current
+        when (configuration.orientation) {
+            ORIENTATION_LANDSCAPE -> {
+                Column {
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Appetizers",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Purple80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Salads",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .padding(menuPadding)
+                        )
+                    }
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Drinks",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Pink80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Mains",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(PurpleGrey80)
+                                .padding(menuPadding)
+                        )
+                    }
+                }
+            }
+
+            else -> {
+                Column {
+                    Text(
+                        "Appetizers",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Purple80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Salads",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Drinks",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Pink80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Mains",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(PurpleGrey80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                }
+            }
+        }
     }
+
 }
